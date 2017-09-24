19d19df0b62a559df2df91dda2f2a4feca447e57
pranjulps3 <pranjul.ps3@gmail.com>
2017-09-25 04:16:47 +0530

    Minor changes

--- login/static/autocomplete_light/jquery.init.js
+++
@@ -1,9 +0,0 @@
-var yl = yl || {};
-
-if (yl.jQuery === undefined) {
-    if ((typeof django !== 'undefined') && (typeof django.jQuery !== 'undefined'))
-        yl.jQuery = django.jQuery;
-
-    else if (typeof $ !== 'undefined')
-        yl.jQuery = $;
-}
